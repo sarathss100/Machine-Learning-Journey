0606da8a704c8daa9a2f0bf88f1ee914dc0b8f48
Sarath S S <sarathss100@gmail.com>
2024-10-27 12:25:47 +0530

    Day-1 task completed project-Band Name Generator

---
+++ test.py
@@ -0,0 +1,23 @@
+# print("Hello World!")
+
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to rise for 2 hours.")
+# print("5. Bake at 200 degrees C for 30 minutes.")
+
+# print("Hello \nWorld!")
+
+# print("Hello" + "Means")
+
+# print("Hello " + input("What is your name?\n")+"!")
+
+# Python variables
+
+# name = input("What is your name?\n")
+# name = "Mumu"
+# print(name)
+
+# name = "Hello"
+# print(len(name))
+
